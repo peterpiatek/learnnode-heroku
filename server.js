2a0abe0c1496cf18b52addc356e2f3bf7a11929a
abc <abc@example.com>
2018-10-19 00:47:33 +0100

    maintenance page

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const ENV_maintenance = false;
 
 //set hbs as view engine 
 app.set('view engine', 'hbs');
@@ -27,12 +30,32 @@ hbs.registerHelper('uppercase', (text) => {
 // needs to be moved under other middlewares as it is important where it resides lexically
 app.use(express.static(__dirname + '/public'));
 
+// using middleware for log changes storage
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let logData = `${now}: ${req.method}, ${req.url} \n`; 
+  fs.appendFile('server-log', logData, (err) =>{
+    if(err) console.log('Unable to update the log file');
+  })
+  next();
+})
+
+// using middleware for maintenance page 
+if(ENV_maintenance){
+  app.use((req, res) => {
+    res.render('maintenance.html', {
+      pageTitle: 'Sorry we are updating the website'
+    })
+  })
+}
 
 app.get('/', (req, res) => [
   res.render('home.html', {
     pageTitle: 'Home page'
   })
 ])
+
+// app.get('projects')
 
 app.get('/about', (req, res) => [
   res.render('about.html', {
